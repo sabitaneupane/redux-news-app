23e17250a1da2abea89a49100fa0e573799cca36
Sabita Neupane <nsabita.sasa@gmail.com>
2019-09-09 17:20:39 +0545

    Moving store

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/App';
 
-import store from "./store";
+import store from "./sagas/store";
 import { Provider } from "react-redux";
 
 ReactDOM.render(

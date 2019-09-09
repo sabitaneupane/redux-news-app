a8c75e5df80cdf1b528f28b493c583e5aaa3b4c5
Sabita Neupane <nsabita.sasa@gmail.com>
2019-09-09 17:27:11 +0545

    Refactoring store and sagas

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from 'redux-saga';
 import reducer from "../reducers";
-import rootSaga from '.';
+import rootSaga from './saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
